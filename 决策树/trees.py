aee001413a78034b905fa410c45381d482dffb60
zl.yang@yunding360.com <xueran520>
2019-04-22 13:16:02 +0800

    决策树

---
+++ 决策树/trees.py
@@ -0,0 +1,27 @@
+from math import log
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+            [1, 1, 'yes'],
+            [1, 0, 'no'],
+            [0, 1, 'no'],
+            [0, 1, 'no']]
+    labels = ['不浮出水面是否可以生存', '是否有脚蹼']
+    return dataSet, labels
+
+# 计算给定数据集的香农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+
